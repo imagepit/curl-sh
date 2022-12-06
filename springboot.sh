bd16f1c82ab0005331d083bbb0ef3c7fa0df6c4a
Ryosuke Takahashi <r-takahashi@image-pit.com>
2022-12-06 22:49:42 +0900

    Update springboot.sh

--- springboot.sh
+++ springboot.sh
@@ -1,4 +1,5 @@
 sudo apt update && sudo apt install -y zip unzip openjdk-17-jdk
 curl -s "https://get.sdkman.io" | bash
-bash $HOME/.sdkman/bin/sdkman-init.sh; sdk install springboot
+echo ". $HOME/.sdkman/bin/sdkman-init.sh; sdk install springboot" | bash
 sdk install springboot
+sh $HOME/.sdkman/bin/sdkman-init.sh
