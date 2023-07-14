6d9199aeadbc46116a0c9367d6e3952a4e5cdb2d
Ryosuke Takahashi <r-takahashi@image-pit.com>
2023-07-14 19:39:42 +0900

    update k8s.sh

--- k8s.sh
+++ k8s.sh
@@ -1,6 +1,6 @@
 # docker-compose
 sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chown vagrant:vagrant /usr/local/bin/docker-compose
+sudo chown $USER:$USER /usr/local/bin/docker-compose
 sudo chmod 775 /usr/local/bin/docker-compose
 
 # kubectl
@@ -9,13 +9,13 @@ chmod +x ./kubectl
 sudo mv ./kubectl /usr/local/bin/kubectl
 
 # kind
-curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.17.0/kind-linux-amd64
+curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
 chmod +x ./kind
 sudo mv ./kind /usr/local/bin/kind
 
 # nfs-server
-sudo apt install -y nfs-kernel-server
-sudo mkdir -p /var/nfs/exports
-sudo chown nobody.nogroup /var/nfs/exports
-echo "/var/nfs/exports *(rw,sync,fsid=0,crossmnt,no_subtree_check,insecure,all_squash)" >> /etc/exports
-sudo exportfs -ra
+# sudo apt install -y nfs-kernel-server
+# sudo mkdir -p /var/nfs/exports
+# sudo chown nobody.nogroup /var/nfs/exports
+# echo "/var/nfs/exports *(rw,sync,fsid=0,crossmnt,no_subtree_check,insecure,all_squash)" >> /etc/exports
+# sudo exportfs -ra
