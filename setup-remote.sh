eb0d2ca99086d29fb89bc9691388d6516e88cb50
Ryosuke Takahashi <r-takahashi@image-pit.com>
2024-01-31 20:05:21 +0900

    add setup-remote.sh

---
+++ setup-remote.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# Switch to root
+sudo su -
+
+# Install Cloudflare CLI
+wget https://github.com/cloudflare/cloudflared/releases/latest/download/cloudflared-linux-amd64.deb
+sudo dpkg -i cloudflared-linux-amd64.deb
+
+# Download Cloudflare Settings
+docker run --rm -v .:/root imagepit/vsremo vsremo set $1
+
+# Run Cloudflare Tunnel
+sudo cloudflared --config ~/.cloudflared/config.yml service install
