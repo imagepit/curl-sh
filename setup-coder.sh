0502e9782cd28f8ac58efbe9d39716282665bed8
Ryosuke Takahashi <r-takahashi@image-pit.com>
2024-02-25 18:51:18 +0900

    add setup coder and kind

---
+++ setup-coder.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+pcid=$1
+pass=$2
+
+# Docker
+sudo curl https://get.docker.com | sh
+sudo usermod -aG docker $USER
+
+# code-server
+curl -fsSL https://code-server.dev/install.sh | sh
+sudo systemctl enable --now code-server@$USER
+# 1秒待つ
+sleep 1
+# .config/code-server/config.yamlをsedコマンドで編集 127.0.0.1:8080 -> 0.0.0.0:9999、password: "password" -> password: "imagepit"
+sed -i -e 's/bind-addr: 127.0.0.1:8080/bind-addr: 0.0.0.0:9999/g' ~/.config/code-server/config.yaml
+sed -i -e 's/password: .*/password: $pass/g' ~/.config/code-server/config.yaml
+sudo systemctl restart code-server@$USER
+sudo systemctl status code-server@$USER
+
+# cloudflared tunnel
+curl -s https://raw.githubusercontent.com/imagepit/curl-sh/master/setup-remote.sh | bash -s $pcid
---
+++ setup-kind.sh
@@ -0,0 +1,65 @@
+# kind
+[ $(uname -m) = x86_64 ] && curl -Lo ./kind https://kind.sigs.k8s.io/dl/v0.20.0/kind-linux-amd64
+chmod +x ./kind
+sudo mv ./kind /usr/local/bin/kind
+
+# kubectl
+curl -LO "https://dl.k8s.io/release/$(curl -LS https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# helm3
+curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash
+
+# ホストのIPアドレスを変数に格納
+export HOST_IP=$(hostname -I | awk '{print $1}')
+
+# teeコマンドでkind/clust.yamlを作成
+mkdir -p kind
+cat <<EOF | tee kind/cluster.yaml
+kind: Cluster
+apiVersion: kind.x-k8s.io/v1alpha4
+networking:
+  apiServerAddress: $HOST_IP
+  apiServerPort: 6443
+nodes:
+- role: control-plane
+  extraPortMappings:
+  - containerPort: 80
+    hostPort: 80
+    protocol: TCP
+  - containerPort: 443
+    hostPort: 443
+    protocol: TCP
+  - containerPort: 8080
+    hostPort: 8080
+    protocol: TCP
+  - containerPort: 30080
+    hostPort: 30080
+  - containerPort: 30090
+    hostPort: 30090
+  - containerPort: 30100
+    hostPort: 30100
+    protocol: TCP
+  extraMounts:
+    - hostPath: ./data
+      containerPath: /var/local-path-provisioner
+- role: worker
+- role: worker
+kubeadmConfigPatches:
+- |
+    kind: InitConfiguration
+    nodeRegistration:
+      kubeletExtraArgs:
+        node-labels: "ingress-ready=true"
+        authorization-mode: "AlwaysAllow"
+EOF
+
+# kindクラスタの作成
+sudo kind create cluster --config kind/cluster.yaml --name kind
+
+# KubernetesノードがREADYになるまで待機
+kubectl wait --for=condition=Ready node --all --timeout=300s
+
+# ノード一覧の表示
+kubectl get nodes
